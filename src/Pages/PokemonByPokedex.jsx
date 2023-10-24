4b3f42a2da5d6a2824fe41ec6b84f8cc5d0b86c6
GaranceZ <zajdlicgarance@gmail.com>
2023-10-24 12:05:47 +0200

    Pokemons par pokédex régional fonctionnel

--- src/Pages/PokemonByPokedex.jsx
+++ src/Pages/PokemonByPokedex.jsx
@@ -2,20 +2,30 @@ import React, { useState, useEffect } from 'react';
 import pokedexRegionServices from '../Services/pokedexRegionServices';
 import NavbarPokemon from '../Components/NavbarPokemon';
 import { useParams } from 'react-router-dom';
-import Pokemons from "../Components/Pokemon"
+import Pokemons from "../Components/Pokemon";
+import Loading from "../Components/Loading";
 
 const PokemonByPokedex = () => {
 
     const {id} = useParams();
     const [pokePokedex, setPokePokedex] = useState({});
+    const [loading, setLoading] = useState(true);
 
     const fetchPokemonByPokedex = async () => {
         try {
             //const response = await pokedexRegionServices.getPokedex(id);
             const res = await pokedexRegionServices.getPokedexById(id);
-            
-            setPokePokedex(res.data.pokemon_entries);
-            console.log(res.data.pokemon_entries);
+            const tabName = [];
+            for (var i = 0; i < res.data.pokemon_entries.length; i++) {
+                var name = res.data.pokemon_entries[i].pokemon_species;
+                tabName.push(name);
+              }
+            tabName.sort((firstItem, secondItem) =>
+            firstItem.url.substring(41).replaceAll( "/", "") - secondItem.url.substring(41).replaceAll( "/", ""))
+            //console.log(tabName);
+            setPokePokedex(tabName);
+            setLoading(false)
+            //console.log(res.data.pokemon_entries);
         } catch (e) {
             console.log(e)
         }
@@ -24,19 +34,22 @@ const PokemonByPokedex = () => {
         fetchPokemonByPokedex()
     }, []);
 
+   
 
-    return ( 
-        <>
+
+    return <>
         <NavbarPokemon/>
+        {loading === false ? <>
         <div className={"d-flex flex-wrap justify-content-center gap-2 "}>
-        {pokePokedex.pokemon_species != undefined && pokePokedex.pokemon_species.map(pokedexPage => {
+        {pokePokedex != undefined && pokePokedex.map(pokedexPage => {
             return <>
             <Pokemons key={pokedexPage.name} pokemon={pokedexPage}/>
             </>
         })}
         </div>
+        </>:<Loading/>}
         </>
-     );
-}
- 
+     
+
+    }
 export default PokemonByPokedex;
